fa01d57fad71c71c719e4b97dcaa22131095718f
Aaron Roles <aaron_roles93@hotmail.com>
2016-10-06 14:56:13 +0100

    Working on product class

--- Week05/Thursday/Product.php
+++ Week05/Thursday/Product.php
@@ -1,12 +1,18 @@
 <?php
+
     class Product{
 
         public function __construct($productName, $productPrice){
-            echo $productName . " cost &euro;" . $productPrice . "<br/>";
+            echo '<div id="product_' . $productName . '">';
+            echo $productName . " - &euro;" . $productPrice . "<br/>";
+            echo '<img src="images/' . $productName . '.jpg" width="250" height="165"><br/>';
+            echo '<button>Add to cart</button><br/>';
+            echo '</div>';
         }
     }
 
     $prd1 = new Product("Shoes", 90);
     $prd2 = new Product("Pants", 20);
-    $prd3 = new Product("Shirt", 15);
+    $prd3 = new Product("Shirt", 25);
+
 ?>
