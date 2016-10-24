b506b4ea2d836f9001262d291eae3abc2c11e2b7
Aaron Roles <aaron_roles93@hotmail.com>
2016-10-24 20:20:39 +0100

    Started work on shopping cart assignment

--- Week07/login-system/login.php
+++ Week07/login-system/login.php
@@ -1,4 +1,5 @@
 <?php
+
     try{
         $conn = new PDO("mysql:host=localhost;dbname=logintest", "root", "");
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -8,27 +9,60 @@
         echo 'ERROR: '.$e->getMessage();
     }
 
+     if(isset($_POST["login"])){
 
-     if(isset($_POST["submit"])){
          // Get username and password 
          $username = $_POST["username"];
          $password = $_POST["password"];
+
         // Check it against a database
         $stmt = $conn->prepare('SELECT username FROM users WHERE username = :username AND password = SHA1(:password)');
         $stmt->bindParam(":username", $username);
         $stmt->bindParam(":password", $password);
         $stmt->execute();
-        while ($row = $stmt->fetch()){
-            var_dump($row);
+
+         // If we find a matching user, then success
+         if($stmt->rowCount() == 1){
+            // Let the user know they were successful
+            //echo "user found :)";
+            // Start a session
+            session_start(); 
+            $_SESSION["userSession"] = $username." is logged in";
+            echo $_SESSION["userSession"];
         }
-         // If true then successful login
-         // If false then error 
+         // Otherwise we have no match, error 
+        else{
+            // Let user know there was an error
+            echo "Username/Password not found";
+        } 
+     }
+
+     if(isset($_POST["logout"])){
+         session_start(); 
+         //echo "Logging out";
+         unset($_SESSION["userSession"]);
+         //session_destroy();
      }
 ?>
 
-<h1>Log In</h1>
-<form method="POST" id="userLogin">
-    <input type="text" id="username" name="username" placeholder="Username..."><br/>
-    <input type="password" id="password" name="password" placeholder="Password..."><br/>
-    <input type="submit" id="submit" name="submit" value="Log In"><br/>
-</form>
+<body>
+    <!-- If session does not exist 
+    then display the log in form -->
+    <?php if (empty($_SESSION["userSession"])) { ?>
+        <h1>Log In</h1>
+        <form method="POST" id="userLogin">
+            <input type="text" id="username" name="username" placeholder="Username..."><br/>
+            <input type="password" id="password" name="password" placeholder="Password..."><br/>
+            <input type="submit" id="login" name="login" value="Log In"><br/>
+        </form> 
+    <?php } ?>
+
+    <!-- Otherwise if the session does exist 
+    then tell the user they are logged in -->
+    <?php if (!empty($_SESSION["userSession"])) { ?>
+        <h1>Logged In</h1>
+        <form method="POST" id="userLogout">
+            <input type="submit" id="logout" name="logout" value="Log Out"><br/>
+        </form> 
+    <?php } ?>
+</body>
