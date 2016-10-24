9ddbbcb9338cdd9fe110c3424d224c5cc40d1249
Aaron Roles <aaron_roles93@hotmail.com>
2016-10-24 20:24:44 +0100

    Shopping cart beginnings

--- ShoppingCart/index.php
+++ ShoppingCart/index.php
@@ -1,10 +1,3 @@
 <?php
-    try{
-        $db = new PDO("mysql:host=localhost;dbname=shopping", "root", "");
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "Connected to db";
-    }
-    catch(PDOException $e){
-        echo "Error - ".$e->getMessage();
-    }
+    include("database.php");
 ?>
