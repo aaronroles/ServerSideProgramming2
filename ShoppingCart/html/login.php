cfa9e7258d903352c0f50d75e1c1c1acb7809d31
Aaron Roles <aaron_roles93@hotmail.com>
2016-10-24 20:44:53 +0100

    Developing login/register for users

---
+++ ShoppingCart/html/login.php
@@ -0,0 +1,8 @@
+<div id="loginUser">
+    <h1>Login</h1>
+    <form method="POST" id="loginForm">
+        <input type="text" id="username" name="username" placeholder="Username"><br/>
+        <input type="password" id="password" name="password" placeholder="Password"><br/>
+        <input type="submit" id="submit" name="submit" value="Login"><br/>
+    </form>
+</div>
---
+++ ShoppingCart/html/register.php
@@ -0,0 +1,11 @@
+<div id="registerUser">
+    <h1>Register</h1>
+    <form method="POST" id="registerForm">
+        <input type="text" id="username" name="username" placeholder="Username"><br/>
+        <input type="password" id="password" name="password" placeholder="Password"><br/>
+        <input type="email" id="email" name="email" placeholder="Email"><br/>
+        <input type="text" id="firstname" name="firstname" placeholder="First Name"><br/>
+        <input type="text" id="lastname" name="lastname" placeholder="Last name"><br/>
+        <input type="submit" id="submit" name="submit" value="Register"><br/>
+    </form>
+</div>
