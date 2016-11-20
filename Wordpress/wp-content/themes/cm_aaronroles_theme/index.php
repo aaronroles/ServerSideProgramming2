e507c81d5719f89016d1db5eba98f5c42fc0f769
Aaron Roles <aaron_roles93@hotmail.com>
2016-11-20 18:38:55 +0000

    Detect admin login

--- Wordpress/wp-content/themes/cm_aaronroles_theme/index.php
+++ Wordpress/wp-content/themes/cm_aaronroles_theme/index.php
@@ -1,9 +1,18 @@
 <?php
+    global $wpdb;
+
     get_header();
     include("html/login.php");
     get_footer();
 
     if(isset($_POST["submitLogin"])){
-        echo "logged in";
+        $username = $_POST["username"];
+        $password = SHA1($_POST["password"]);
+        
+        $data = $wpdb->get_row(
+            "SELECT * FROM ar_users WHERE empUsername='".$username."' AND empPassword='".$password."'"
+        );
+
+        echo "Hello ".$data->empUsername;
     }
 ?>
