b0d186f7bd2c3f75be315a64fd5ed36a5bbfacc7
Aaron Roles <aaron_roles93@hotmail.com>
2016-11-20 17:57:38 +0000

    Login page look

--- Wordpress/wp-content/themes/cm_aaronroles_theme/html/login.php
+++ Wordpress/wp-content/themes/cm_aaronroles_theme/html/login.php
@@ -1,6 +1,6 @@
 <div id="loginUser">
-    <h1 id="loginHeading">Employee Login</h1>
-    <form method="POST" id="loginForm" name="loginForm">
+    <h1 id="loginHeading" class="center">Employee Login</h1>
+    <form method="POST" id="loginForm" name="loginForm" class="center">
         <input type="text" id="username" name="username" placeholder="Username"><br/>
         <input type="password" id="password" name="password" placeholder="Password"><br/>
         <input type="submit" id="submit" name="submitLogin" value="Login"><br/>
