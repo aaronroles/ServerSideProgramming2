0b8ed59c4b3ea42f259ea44cfc0429f36d3dc678
Aaron Roles <aaron_roles93@hotmail.com>
2016-11-21 12:03:04 +0000

    Comments

---
+++ Wordpress/wp-content/themes/cm_aaronroles_theme/html/admin.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<div id="admin">
+    <h1>Welcome Admin</h1>
+    <ul>
+        <li>View all expenses</li>
+        <li>Pending expenses</li>
+        <li>Register new employee</li>
+    </ul>
+</div>
--- Wordpress/wp-content/themes/cm_aaronroles_theme/index.php
+++ Wordpress/wp-content/themes/cm_aaronroles_theme/index.php
@@ -6,13 +6,24 @@
     get_footer();
 
     if(isset($_POST["submitLogin"])){
+        // Get post data
         $username = $_POST["username"];
         $password = SHA1($_POST["password"]);
         
+        // Run a query to find single user row
         $data = $wpdb->get_row(
             "SELECT * FROM ar_users WHERE empUsername='".$username."' AND empPassword='".$password."'"
         );
 
-        echo "Hello ".$data->empUsername;
+        // If we have data
+        if(isset($data)){
+            // Message to logged in user 
+            echo "Hello ".$data->empUsername;
+        }
+        // If no data found
+        else{
+            // Error message
+            echo "No user found";
+        }
     }
 ?>
